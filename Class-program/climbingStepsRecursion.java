bc24c3f8ea559026ae035068698a4e85d977799d
prashant4840 <prashantsharma4840@icloud.com>
2026-03-23 21:58:50 +0530

    Recursion.class

---
+++ Class-program/climbingStepsRecursion.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Recursion {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("n: ");
+        int n = sc.nextInt();
+        System.out.println("steps: " + climbStairs(n));
+    }
+    public static int climbStairs(int n) {
+        if (n <= 2) return n;
+        return climbStairs(n - 1) + climbStairs(n - 2);
+    }
+}
