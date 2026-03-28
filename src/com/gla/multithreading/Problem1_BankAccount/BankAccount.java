ed30692b0d8fd3a782f88b7028525f94d094d53c
prashant4840 <prashantsharma4840@icloud.com>
2026-03-28 18:55:39 +0530

    Bank Account Simulation

---
+++ src/com/gla/multithreading/Problem1_BankAccount/BankAccount.java
@@ -0,0 +1,17 @@
+package com.gla.multithreading;
+
+public class BankAccount {
+    private String accountHolder;
+
+    public BankAccount(String accountHolder) {
+        this.accountHolder = accountHolder;
+    }
+
+    public String getAccountHolder() {
+        return accountHolder;
+    }
+
+    public void checkBalance(String userType) {
+        System.out.println(userType + " user (" + accountHolder + ") is checking balance...");
+    }
+}
